9a4d3926f85a159635238e221543de305ddd4321
zhaomaosheng <zmshahaha@gmail.com>
2024-10-22 14:08:11 +0800

    part a task 2

--- classify/src/relu.s
+++ classify/src/relu.s
@@ -14,25 +14,22 @@
 # ==============================================================================
 relu:
     # Prologue
-
+    bgt a1, zero, loop_start
+    li a0, 36
+    j exit
 
 loop_start:
-
-
-
-
-
-
-
+    addi a1, a1, -1
+    lw t0, 0(a0)
+    bge t0, zero, loop_continue
+    sw zero, 0(a0)
 
 loop_continue:
-
-
+    addi a0, a0, 4
+    bgt a1, zero, loop_start
 
 loop_end:
-
-
     # Epilogue
-
+    li a0, 0
 
     jr ra
