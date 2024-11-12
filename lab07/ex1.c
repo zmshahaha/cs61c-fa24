3fdf98481c122f5b2658b959f074c535f1c9ff9a
zhaomaosheng <zmshahaha@gmail.com>
2024-11-12 15:41:21 +0800

    lab07 fini

--- lab07/ex1.c
+++ lab07/ex1.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <x86intrin.h>
 #include "ex1.h"
+#include <assert.h>
 
 long long int sum(int vals[NUM_ELEMS]) {
     clock_t start = clock();
@@ -46,38 +47,78 @@ long long int sum_unrolled(int vals[NUM_ELEMS]) {
 }
 
 long long int sum_simd(int vals[NUM_ELEMS]) {
-    clock_t start = clock();
-    __m128i _127 = _mm_set1_epi32(127); // This is a vector with 127s in it... Why might you need this?
-    long long int result = 0; // This is where you should put your final result!
-    /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
+	clock_t start = clock();
+	__m128i _127 = _mm_set1_epi32(127);		// This is a vector with 127s in it... Why might you need this?
+	long long int result = 0;				   // This is where you should put your final result!
+	/* DO NOT DO NOT DO NOT DO NOT WRITE ANYTHING ABOVE THIS LINE. */
 
-    for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-        /* YOUR CODE GOES HERE */
-
-        /* Hint: you'll need a tail case. */
-    }
-
-    /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
-    clock_t end = clock();
-    printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
-    return result;
+	int results[4];
+	for(int w = 0; w < OUTER_ITERATIONS; w++) {
+		/* YOUR CODE GOES HERE */
+		int i;
+		__m128i tmp_sum = _mm_setzero_si128();
+		for (i = 0; i + 4 <= NUM_ELEMS; i+=4) {
+			__m128i tmp = _mm_loadu_si128((__m128i*)(vals + i));
+			__m128i mask = _mm_cmpgt_epi32(tmp, _127);
+			tmp = _mm_and_si128(tmp, mask);
+			tmp_sum = _mm_add_epi32(tmp, tmp_sum);
+		}	
+		_mm_storeu_si128((__m128i*)results, tmp_sum);
+		/* You'll need a tail case. */
+		for (; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) results[0] += vals[i];
+		}
+		for (int j = 0; j < 4; j++) {
+			result += results[j];
+		}
+	}
+	clock_t end = clock();
+	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+	return result;
 }
 
 long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
-    clock_t start = clock();
-    __m128i _127 = _mm_set1_epi32(127);
-    long long int result = 0;
-    /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
+	clock_t start = clock();
+	__m128i _127 = _mm_set1_epi32(127);
+	long long int result = 0;
 
-    for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
-        /* YOUR CODE GOES HERE */
-        /* Copy your sum_simd() implementation here, and unroll it */
+	int results[4];
+	for(int w = 0; w < OUTER_ITERATIONS; w++) {
+		/* COPY AND PASTE YOUR sum_simd() HERE */
+		/* MODIFY IT BY UNROLLING IT */
+		int i;
+		__m128i tmp_sum = _mm_setzero_si128();
+		for (i = 0; i + 16 <= NUM_ELEMS; i+=16) {
+			__m128i tmp = _mm_loadu_si128((__m128i*)(vals + i));
+			__m128i mask = _mm_cmpgt_epi32(tmp, _127);
+			tmp = _mm_and_si128(tmp, mask);
+			tmp_sum = _mm_add_epi32(tmp, tmp_sum);
 
-        /* Hint: you'll need 1 or maybe 2 tail cases here. */
-    }
+			tmp = _mm_loadu_si128((__m128i*)(vals + i + 4));
+			mask = _mm_cmpgt_epi32(tmp, _127);
+			tmp = _mm_and_si128(tmp, mask);
+			tmp_sum = _mm_add_epi32(tmp, tmp_sum);
 
-    /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
-    clock_t end = clock();
-    printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
-    return result;
+			tmp = _mm_loadu_si128((__m128i*)(vals + i + 8));
+			mask = _mm_cmpgt_epi32(tmp, _127);
+			tmp = _mm_and_si128(tmp, mask);
+			tmp_sum = _mm_add_epi32(tmp, tmp_sum);
+
+			tmp = _mm_loadu_si128((__m128i*)(vals + i + 12));
+			mask = _mm_cmpgt_epi32(tmp, _127);
+			tmp = _mm_and_si128(tmp, mask);
+			tmp_sum = _mm_add_epi32(tmp, tmp_sum);
+		}	
+		_mm_storeu_si128((__m128i*)results, tmp_sum);
+		/* You'll need 1 or maybe 2 tail cases here. */
+		for (; i < NUM_ELEMS; i++) {
+			if (vals[i] >= 128) results[0] += vals[i];
+		}
+		for (int j = 0; j < 4; j++) {
+			result += results[j];
+		}
+	}
+	clock_t end = clock();
+	printf("Time taken: %Lf s\n", (long double)(end - start) / CLOCKS_PER_SEC);
+	return result;
 }
--- lab07/ex2.c
+++ lab07/ex2.c
@@ -12,6 +12,17 @@ double dotp_critical(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the critical keyword here!
+
+    // Parallel for loop with a critical section to update global_sum
+    #pragma omp parallel for
+    for (int i = 0; i < arr_size; i++) {
+        // Each thread computes a partial sum, but only one thread
+        // can update global_sum at a time
+        #pragma omp critical
+        {
+            global_sum += x[i] * y[i];
+        }
+    }
     return global_sum;
 }
 
@@ -20,6 +31,12 @@ double dotp_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Use the reduction keyword here!
+
+    // Parallel for loop with reduction on global_sum
+    #pragma omp parallel for reduction(+:global_sum)
+    for (int i = 0; i < arr_size; i++) {
+        global_sum += x[i] * y[i];
+    }
     return global_sum;
 }
 
@@ -28,5 +45,24 @@ double dotp_manual_reduction(double* x, double* y, int arr_size) {
     double global_sum = 0.0;
     // TODO: Implement this function
     // Do NOT use the `reduction` directive here!
+
+    #pragma omp parallel
+    {
+        // Each thread has a private partial sum
+        double local_sum = 0.0;
+
+        // Each thread calculates its partial dot product
+        #pragma omp for
+        for (int i = 0; i < arr_size; i++) {
+            local_sum += x[i] * y[i];
+        }
+
+        // Critical section to safely update the global sum
+        #pragma omp critical
+        {
+            global_sum += local_sum;
+        }
+    }
+
     return global_sum;
 }
